354198cf12f4b239aaa065815d534f2b19424d6e
Elina <elinakolod@gmail.com>
2018-06-07 13:32:12 +0300

    fix bugs

--- factory.rb
+++ factory.rb
@@ -1,9 +1,13 @@
 class Factory
+  include Enumerable
+
   def self.new(*attributes, keyword_init: false, &block)
-    flag = attributes[0].is_a?(String) && attributes[0].match(/[A-Z]/)
-    class_name = flag ? attributes.shift : nil
+    first_attribute = attributes[0]
+    flag = first_attribute.is_a?(String) && first_attribute.match(/[A-Z]/)
+    class_name = flag ? attributes.shift : 'not given'
+    attributes_size = attributes.size
+    sorted_attributes = attributes.sort
     subclass = Class.new self do
-      class_eval(&block) if block_given?
       send(:attr_accessor, *attributes)
 
       class << self
@@ -14,25 +18,30 @@ class Factory
         end
       end
       define_method :initialize do |*values|
+        values_size = values.size
+        given_values = values[0]
+        sorted_given_keys = given_values.keys.sort
         if !keyword_init
-          raise ArgumentError, 'Too many arguments' if values.size > attributes.size
-          raise ArgumentError, 'Wrong number of arguments' if values.size < attributes.size
-          values.each_with_index { |val, i| send("#{attributes[i]}=", val) }
-        elsif values[0].keys.sort != attributes.sort
-          redundant_args = values[0].keys.sort - attributes.sort
-          missing_args = attributes.sort - values[0].keys.sort
+          raise ArgumentError, 'Too many arguments' if values_size > attributes_size
+          raise ArgumentError, 'Wrong number of arguments' if values_size < attributes_size
+          index = 0
+          while index < values_size
+            send("#{attributes[index]}=", values[index])
+            index += 1
+          end
+        elsif sorted_given_keys != sorted_attributes
+          redundant_args = sorted_given_keys - sorted_attributes
+          missing_args = sorted_attributes - sorted_given_keys
           raise ArgumentError, "Do not match expected keys: #{missing_args}, #{redundant_args}"
         else
-          values[0].map { |k, v| instance_variable_set "@#{k}", v }
+          given_values.map { |k, v| instance_variable_set "@#{k}", v }
         end
       end
 
       define_method :[] do |member|
-        if member.is_a?(String) || member.is_a?(Symbol)
-          return instance_variable_get("@#{member}")
-        end
+        return instance_variable_get("@#{member}") if member.is_a?(String) || member.is_a?(Symbol)
         if member.is_a? Integer
-          raise IndexError if member > attributes.size
+          raise IndexError if member > attributes_size
           return to_a[member]
         end
       end
@@ -49,7 +58,8 @@ class Factory
         members
       end
     end
-    class_name.nil? ? subclass : Object.const_set(class_name, subclass)
+    class_name == 'not given' ? subclass : const_set(class_name, subclass)
+    class_eval(&block) if block_given?
   end
 
   def ==(other)
